b05ca6b390da49785d5a4cffa5a94d9268d48ab4
Reese2596 <reese2596@student.cptc.edu>
2020-02-19 14:32:38 -0800

    Created inheritance class

---
+++ Inheritance/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Product
+    {
+        public String code { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{code} : {Price}";
+        }
+    }
+}
